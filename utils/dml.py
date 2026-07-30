4f12230239908e27754ab2e0c27f41cf2c1213b9
Samuel Fedora <spalominor@unal.edu.co>
2026-07-29 23:38:50 -0500

    feat: create dml script

--- utils/dml.py
+++ utils/dml.py
@@ -17,7 +17,6 @@ POSTGRES_CREDENTIALS = {
 }
 
 POSTGRES_URL = f"postgresql://{POSTGRES_CREDENTIALS['user']}:{POSTGRES_CREDENTIALS['password']}@{POSTGRES_CREDENTIALS['host']}:{POSTGRES_CREDENTIALS['port']}/{POSTGRES_CREDENTIALS['database']}"
-print(POSTGRES_URL)
 
 MYSQL_CREDENTIALS = {
     "host": os.getenv("MYSQL_HOST"),
@@ -26,3 +25,92 @@ MYSQL_CREDENTIALS = {
     "user": os.getenv("MYSQL_USER"),
     "password": os.getenv("MYSQL_PASSWORD")  
 }
+
+
+# per default, the select all is in the MySQL database
+# TODO: add a parameter to select the database (MySQL or PostgreSQL)
+# generics functions with params
+def select_all_mysql():
+    with cpy.connect(**MYSQL_CREDENTIALS) as cnx:
+        with cnx.cursor() as cur:
+            cur.execute("SELECT * FROM mercancia;")
+            mercancia_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM viaje;")
+            viaje_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM buque;")
+            buque_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM naviera;")
+            naviera_rows = cur.fetchall()
+    
+    return {
+        "mercancia": mercancia_rows,
+        "viaje": viaje_rows,
+        "buque": buque_rows,
+        "naviera": naviera_rows
+    }
+    
+    
+def select_all_postgres():
+    with psycopg2.connect(POSTGRES_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM mercancia;")
+            mercancia_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM viaje;")
+            viaje_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM buque;")
+            buque_rows = cur.fetchall()
+            
+            cur.execute("SELECT * FROM naviera;")
+            naviera_rows = cur.fetchall()
+    
+    return {
+        "mercancia": mercancia_rows,
+        "viaje": viaje_rows,
+        "buque": buque_rows,
+        "naviera": naviera_rows
+    }
+    
+    
+def query_1_mysql():
+    with cpy.connect(**MYSQL_CREDENTIALS) as cnx:
+        with cnx.cursor() as cur:
+            with open("mysql/query_1.sql", "r") as f:
+                sql_query = f.read()
+                cur.execute(sql_query)
+                result = cur.fetchall()
+    return result
+
+
+def query_2_mysql():
+    with cpy.connect(**MYSQL_CREDENTIALS) as cnx:
+        with cnx.cursor() as cur:
+            with open("mysql/query_2.sql", "r") as f:
+                sql_query = f.read()
+                cur.execute(sql_query)
+                result = cur.fetchall()
+    return result
+
+
+def query_1_postgres():
+    with psycopg2.connect(POSTGRES_URL) as conn:
+        with conn.cursor() as cur:
+            with open("postgres/query_1.sql", "r") as f:
+                sql_query = f.read()
+                cur.execute(sql_query)
+                result = cur.fetchall()
+    return result
+
+
+def query_2_postgres():
+    with psycopg2.connect(POSTGRES_URL) as conn:
+        with conn.cursor() as cur:
+            with open("postgres/query_2.sql", "r") as f:
+                sql_query = f.read()
+                cur.execute(sql_query)
+                result = cur.fetchall()
+    return result
