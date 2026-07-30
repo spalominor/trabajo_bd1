cbf61f026c51e3049a623c16fd7916ed73e6053b
Samuel Fedora <spalominor@unal.edu.co>
2026-07-29 20:37:03 -0500

    fix: optimization of types in postgres ddl

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE NAVIERA (
-    nombre VARCHAR(100) NOT NULL,
-    pais_registro VARCHAR(100) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    pais_registro VARCHAR(20) NOT NULL,
     certificacion_solas BOOLEAN NOT NULL,
     certificacion_ism BOOLEAN NOT NULL,
     CONSTRAINT PK_NAVIERA PRIMARY KEY (nombre)
@@ -8,14 +8,14 @@ CREATE TABLE NAVIERA (
 
 CREATE TABLE BUQUE (
     numero_omi INT NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    bandera VARCHAR(100) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    bandera VARCHAR(20) NOT NULL,
     anio_construccion INT NOT NULL,
-    capacidad_teu INT NOT NULL,
-    eslora NUMERIC(6, 2) NOT NULL,
-    manga NUMERIC(6, 2) NOT NULL,
-    calado_maximo NUMERIC(6, 2) NOT NULL,
-    nombre_naviera VARCHAR(100) NOT NULL,
+    capacidad_teu SMALLINT NOT NULL,
+    eslora INT NOT NULL,
+    manga INT NOT NULL,
+    calado_maximo INT NOT NULL,
+    nombre_naviera VARCHAR(20) NOT NULL,
     CONSTRAINT PK_BUQUE PRIMARY KEY (numero_omi),
     CONSTRAINT FK_BUQUE_NAVIERA FOREIGN KEY (nombre_naviera) 
         REFERENCES NAVIERA (nombre) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -27,11 +27,11 @@ CREATE TABLE BUQUE (
 );
 
 CREATE TABLE VIAJE (
-    codigo VARCHAR(50) NOT NULL,
+    codigo CHAR(10) NOT NULL,
     fecha_estimada_salida TIMESTAMP NOT NULL,
     fecha_estimada_llegada TIMESTAMP NOT NULL,
-    estado VARCHAR(30) NOT NULL,
-    consumo_combustible NUMERIC(10, 2) NOT NULL,
+    estado VARCHAR(20) NOT NULL,
+    consumo_combustible INT NOT NULL,
     buque_numero_omi INT NOT NULL,
     CONSTRAINT PK_VIAJE PRIMARY KEY (codigo),
     CONSTRAINT FK_VIAJE_BUQUE FOREIGN KEY (buque_numero_omi) 
@@ -41,12 +41,12 @@ CREATE TABLE VIAJE (
 );
 
 CREATE TABLE MERCANCIA (
-    codigo VARCHAR(50) NOT NULL,
-    descripcion TEXT NOT NULL,
-    peso_bruto NUMERIC(10, 2) NOT NULL,
-    volumen NUMERIC(10, 2) NOT NULL,
-    pais_origen VARCHAR(100) NOT NULL,
-    codigo_viaje VARCHAR(50) NOT NULL,
+    codigo CHAR(10) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    peso_bruto INT NOT NULL,
+    volumen INT NOT NULL,
+    pais_origen VARCHAR(20) NOT NULL,
+    codigo_viaje CHAR(10) NOT NULL,
     CONSTRAINT PK_MERCANCIA PRIMARY KEY (codigo),
     CONSTRAINT FK_MERCANCIA_VIAJE FOREIGN KEY (codigo_viaje) 
         REFERENCES VIAJE (codigo) ON DELETE RESTRICT ON UPDATE CASCADE,
